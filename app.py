81aa37c720c675216dfe3bdf0bcd1161dbeb7318
Anastasios3 <117446378+Anastasios3@users.noreply.github.com>
2024-06-20 21:09:03 +0300

    chore: Remove unused code and styles

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
 from flask import Flask, render_template, redirect, url_for, request, flash
-from extensions import db, login_manager
-from flask_login import UserMixin, login_user, login_required, logout_user, current_user
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yoursecretkey'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///task_manager.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Suppress warning
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db.init_app(app)
-login_manager.init_app(app)
-login_manager.login_view = 'login'  # Redirects to the login page if the user is not logged in
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
-# Import models and forms after initializing db and login_manager to avoid circular imports
 from models import User, Task
 from forms import RegistrationForm, LoginForm, TaskForm
 
@@ -21,7 +20,7 @@ def load_user(user_id):
 
 @app.route('/')
 def home():
-    return redirect(url_for('login'))  # Redirects to the login page by default
+    return redirect(url_for('login'))
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -57,43 +56,45 @@ def logout():
 @login_required
 def tasks():
     form = TaskForm()
+    sort_by = request.args.get('sort_by', 'date')
+    order = request.args.get('order', 'asc')
+    priority = request.args.get('priority', None)
+
+    query = Task.query.filter_by(user_id=current_user.id)
+
+    if priority:
+        query = query.filter_by(priority=priority)
+
+    if sort_by == 'priority':
+        if order == 'asc':
+            query = query.order_by(Task.priority.asc())
+        else:
+            query = query.order_by(Task.priority.desc())
+    else:  # Default is sorting by date
+        if order == 'asc':
+            query = query.order_by(Task.id.asc())
+        else:
+            query = query.order_by(Task.id.desc())
+
+    tasks = query.all()
+
     if form.validate_on_submit():
-        new_task = Task(title=form.title.data, description=form.description.data, owner=current_user)
-        db.session.add(new_task)
-        db.session.commit()
-        flash('Task created!', 'success')
+        new_task = Task(
+            title=form.title.data,
+            description=form.description.data,
+            priority=form.priority.data,
+            owner=current_user
+        )
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            flash('Task created!', 'success')
+        except Exception as e:
+            db.session.rollback()
+            flash('Error creating task: {}'.format(e), 'danger')
         return redirect(url_for('tasks'))
-    tasks = Task.query.filter_by(user_id=current_user.id).all()
+
     return render_template('tasks.html', tasks=tasks, form=form)
-
-@app.route('/update_task/<int:task_id>', methods=['GET', 'POST'])
-@login_required
-def update_task(task_id):
-    task = Task.query.get_or_404(task_id)
-    if task.owner != current_user:
-        return redirect(url_for('tasks'))
-    form = TaskForm()
-    if form.validate_on_submit():
-        task.title = form.title.data
-        task.description = form.description.data
-        db.session.commit()
-        flash('Task updated!', 'success')
-        return redirect(url_for('tasks'))
-    elif request.method == 'GET':
-        form.title.data = task.title
-        form.description.data = task.description
-    return render_template('update_task.html', form=form)
-
-@app.route('/delete_task/<int:task_id>')
-@login_required
-def delete_task(task_id):
-    task = Task.query.get_or_404(task_id)
-    if task.owner != current_user:
-        return redirect(url_for('tasks'))
-    db.session.delete(task)
-    db.session.commit()
-    flash('Task deleted!', 'success')
-    return redirect(url_for('tasks'))
 
 if __name__ == '__main__':
     app.run(debug=True)
